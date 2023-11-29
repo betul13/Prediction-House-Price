04bece1b1d6cccb54357230d7590a2acd0081ffd
betul13 <betuldogan406@gmail.com>
2023-11-29 16:29:23 +0300

    first commit

---
+++ house_prices.py
@@ -0,0 +1,385 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from catboost import CatBoostRegressor
+from lightgbm import LGBMRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split, cross_val_score,GridSearchCV
+pd.set_option("display.max_columns", None) #tüm sütunları görmek içi
+pd.set_option("display.max_rows", None) #tüm satırları görmek içi
+warnings.simplefilter(action = "ignore", category = Warning) #uyarıları kapadık
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
+from sklearn.model_selection import GridSearchCV, cross_validate, RandomizedSearchCV, validation_curve
+
+
+df_test = pd.read_csv("datasets/test.csv")
+df_train = pd.read_csv("datasets/train.csv")
+
+
+
+df = pd.concat([df_train, df_test], ignore_index=True) #train ve test setini birleştirdik.
+df.head()
+df.describe()
+df.info()
+df.isnull().sum().sum()
+
+
+def grab_col_names(dataframe, cat_th=20, car_th=30): # Numerik ve kategorik değişkenlerin veri içindeki dağılımını gözlemleyiniz.
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+    num_cols = [col for col in num_cols if "Id" not in col]
+
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+for col in cat_cols: #Kategorik değişkenler ile hedef değişken incelemesini yapınız.
+    print(df.groupby(col)["SalePrice"].agg(["mean","sum"]))
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75): #def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+outlier_list = []
+for col in num_cols:
+    if check_outlier(df,col):
+        outlier_list.append(col)
+print(outlier_list)
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+for col in outlier_list:
+    replace_with_thresholds(df,col)
+
+
+null_list = []
+for col in df.columns: #eksik değerlere bakıcaz
+    if df[col].isnull().any():
+        null_list.append(col)
+print(null_list)
+
+
+def missing_values_table(dataframe): #eksik değerleri inceleyelim
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+    return na_columns
+na_columns = missing_values_table(df)
+
+miss_cat = [col for col in na_columns if col in cat_cols]
+miss_num = [col for col in na_columns if col in num_cols]
+
+threshold = 4  # Eksik değer sayısı eşiği
+
+# Her bir sütundaki eksik değer sayısını kontrol et
+for col in miss_cat:
+    if df[col].isnull().sum() <= threshold:
+        # Belirli sütundaki eksik değeri 4'ten az olan satırları kaldır
+        df = df[df[col].notnull()]
+na_columns = missing_values_table(df)
+miss_cat = [col for col in na_columns if col in cat_cols]
+miss_num = [col for col in na_columns if col in num_cols]
+
+for col in miss_cat:
+    df[col].fillna("No", inplace=True)
+
+na_columns = missing_values_table(df)
+miss_cat = [col for col in na_columns if col in cat_cols]
+miss_num = [col for col in na_columns if col in num_cols]
+
+df = df[df['GarageYrBlt'].notnull()]
+
+na_columns = missing_values_table(df)
+miss_cat = [col for col in na_columns if col in cat_cols]
+miss_num = [col for col in na_columns if col in num_cols]
+miss_num = [col for col in na_columns if col not in "SalePrice"]
+
+for col in miss_num:
+    df[col].fillna(df[col].median(),inplace = True)
+
+na_columns = missing_values_table(df)
+miss_num = [col for col in na_columns if col in num_cols]
+
+df.isnull().sum()#kontrolümüzü yaptık eksik ve aykırı değer problemleri çözüldü
+
+
+#############################################
+# Rare Encoding
+#############################################
+
+# 1. Kategorik değişkenlerin azlık çokluk durumunun analiz edilmesi.
+# 2. Rare kategoriler ile bağımlı değişken arasındaki ilişkinin analiz edilmesi.
+# 3. Rare encoder yazacağız.
+
+def rare_analyser(dataframe, target, cat_cols):
+    for col in cat_cols:
+        print(col, ":", len(dataframe[col].value_counts()))
+        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
+                            "RATIO": dataframe[col].value_counts() / len(dataframe),
+                            "TARGET_MEAN": dataframe.groupby(col)[target].mean()}), end="\n\n\n")
+
+rare_analyser(df, "SalePrice", cat_cols)
+
+def rare_encoder(dataframe, rare_perc):
+    temp_df = dataframe.copy()
+
+    rare_columns = [col for col in temp_df.columns if temp_df[col].dtypes == 'O'
+                    and (temp_df[col].value_counts() / len(temp_df) < rare_perc).any(axis=None)]
+
+    for var in rare_columns:
+        tmp = temp_df[var].value_counts() / len(temp_df)
+        rare_labels = tmp[tmp < rare_perc].index
+        temp_df[var] = np.where(temp_df[var].isin(rare_labels), 'Rare', temp_df[var])
+
+    return temp_df
+
+new_df = rare_encoder(df, 0.01)
+
+
+
+
+
+# Toplam alan
+df["NEW_TotalFlrSF"] = df["1stFlrSF"] + df["2ndFlrSF"]
+
+# Bodrum alanı
+df["NEW_TotalBsmtFin"] = df.BsmtFinSF1 + df.BsmtFinSF2
+
+# Veranda alanı
+df["NEW_PorchArea"] = df.OpenPorchSF + df.EnclosedPorch + df.ScreenPorch + df["3SsnPorch"] + df.WoodDeckSF
+
+# Total House Area
+df["NEW_TotalHouseArea"] = df.NEW_TotalFlrSF + df.TotalBsmtSF
+
+# total m2
+df["NEW_TotalSqFeet"] = df.GrLivArea + df.TotalBsmtSF
+
+
+# Lot Ratio
+df["NEW_LotRatio"] = df.GrLivArea / df.LotArea
+
+
+df["NEW_RatioArea"] = df.NEW_TotalHouseArea / df.LotArea
+
+df["NEW_GarageLotRatio"] = df.GarageArea / df.LotArea
+
+
+# MasVnrArea
+df["NEW_MasVnrRatio"] = df.MasVnrArea / df.NEW_TotalHouseArea
+
+# Dif Area
+df["NEW_DifArea"] = (df.LotArea - df["1stFlrSF"] - df.GarageArea - df.NEW_PorchArea - df.WoodDeckSF)
+
+
+df["NEW_OverallGrade"] = df["OverallQual"] * df["OverallCond"]
+
+# Tadilat tarihi - inşa edilme tarihi
+df["NEW_Restoration"] = df.YearRemodAdd - df.YearBuilt
+
+# Ev yaşı
+df["NEW_HouseAge"] = df.YrSold - df.YearBuilt
+
+# Evin yası - restorasyon tarihi
+df["NEW_RestorationAge"] = df.YrSold - df.YearRemodAdd
+
+# Garajın inşa edildigi yıl - evin yaşı
+df["NEW_GarageAge"] = df.GarageYrBlt - df.YearBuilt
+
+# Grajın insa edildigi tarih - restorasyon tarihi
+df["NEW_GarageRestorationAge"] = np.abs(df.GarageYrBlt - df.YearRemodAdd)
+
+# Evin yaşı garaşın inşa edildigi zaman
+df["NEW_GarageSold"] = df.YrSold - df.GarageYrBlt
+
+df.info()
+
+# Label Encoding & One-Hot Encoding işlemlerini uygulayınız.
+
+cat_cols, cat_but_car, num_cols = grab_col_names(df)
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+binary_cols = [col for col in df.columns if df[col].dtypes == "O" and len(df[col].unique()) == 2]
+
+for col in binary_cols:
+    label_encoder(df, col)
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+df = one_hot_encoder(df, cat_cols, drop_first=True)
+
+
+# MODELLEME
+##################################
+
+#  Train ve Test verisini ayırınız. (SalePrice değişkeni boş olan değerler test verisidir.)
+train_df = df[df['SalePrice'].notnull()]
+test_df = df[df['SalePrice'].isnull()]
+
+y = train_df['SalePrice'] # np.log1p(df['SalePrice'])
+X = train_df.drop(["Id", "SalePrice"], axis=1)
+
+# Train verisi ile model kurup, model başarısını değerlendiriniz.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=17)
+
+models = [('LR', LinearRegression()),
+          #("Ridge", Ridge()),
+          #("Lasso", Lasso()),
+          #("ElasticNet", ElasticNet()),
+          ('KNN', KNeighborsRegressor()),
+          ('CART', DecisionTreeRegressor()),
+          ('RF', RandomForestRegressor()),
+          #('SVR', SVR()),
+          ('GBM', GradientBoostingRegressor()),
+          ("XGBoost", XGBRegressor(objective='reg:squarederror')),
+          ("LightGBM", LGBMRegressor())]
+          # ("CatBoost", CatBoostRegressor(verbose=False))]
+
+for name, regressor in models:
+    rmse = np.mean(np.sqrt(-cross_val_score(regressor, X, y, cv=5, scoring="neg_mean_squared_error")))
+    print(f"RMSE: {round(rmse, 4)} ({name}) ")
+
+
+# Log dönüşümünün gerçekleştirilmesi
+
+
+train_df = df[df['SalePrice'].notnull()]
+test_df = df[df['SalePrice'].isnull()]
+
+y = np.log1p(train_df['SalePrice'])
+X = train_df.drop(["Id", "SalePrice"], axis=1)
+
+# Verinin eğitim ve tet verisi olarak bölünmesi
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=17)
+
+
+# lgbm_tuned = LGBMRegressor(**lgbm_gs_best.best_params_).fit(X_train, y_train)
+
+lgbm = LGBMRegressor().fit(X_train, y_train)
+y_pred = lgbm.predict(X_test)
+
+# Yapılan LOG dönüşümünün tersinin (inverse'nin) alınması
+new_y = np.expm1(y_pred)
+new_y_test = np.expm1(y_test)
+np.sqrt(mean_squared_error(new_y_test, new_y))
+
+# hiperparametre optimizasyonları
+
+lgbm_model = LGBMRegressor(random_state=46)
+
+rmse = np.mean(np.sqrt(-cross_val_score(lgbm_model, X, y, cv=5, scoring="neg_mean_squared_error")))
+
+lgbm_params = {"learning_rate": [0.01, 0.1],
+               "n_estimators": [500, 1500]
+               #"colsample_bytree": [0.5, 0.7, 1]
+             }
+
+lgbm_gs_best = GridSearchCV(lgbm_model,
+                            lgbm_params,
+                            cv=3,
+                            n_jobs=-1,
+                            verbose=True).fit(X, y)
+
+lgbm_gs_best.best_params_
+
+final_model = lgbm_model.set_params(**lgbm_gs_best.best_params_).fit(X, y)
+
+rmse = np.mean(np.sqrt(-cross_val_score(final_model, X, y, cv=5, scoring="neg_mean_squared_error")))
+
+
+
+# feature importance
+def plot_importance(model, features, num=20, save=False):
+
+    feature_imp = pd.DataFrame({"Value": model.feature_importances_, "Feature": features.columns})
+    plt.figure(figsize=(10, 10))
+    sns.set(font_scale=1)
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value", ascending=False)[0:num])
+    plt.title("Features")
+    plt.tight_layout()
+    plt.show()
+    if save:
+        plt.savefig("importances.png")
+
+model = LGBMRegressor().fit(X, y)
+plot_importance(model, X)
+
+predictions = model.predict(test_df.drop(["Id","SalePrice"], axis=1))
+
+dictionary = {"Id":test_df.index, "SalePrice":predictions}
+dfSubmission = pd.DataFrame(dictionary)
+dfSubmission.to_csv("housePricePredictions.csv", index=False)
